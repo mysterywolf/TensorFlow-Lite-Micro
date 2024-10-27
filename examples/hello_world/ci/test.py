f08c55c09366d7a3df1bb1fb7e28f4aed31b366e
Meco Man <920369182@qq.com>
2024-10-27 00:57:28 -0400

    tf 2.2.0

--- examples/hello_world/ci/test.py
+++ examples/hello_world/ci/test.py
@@ -1,13 +1,24 @@
-print('hello world')
+# from: https://github.com/RTduino-libraries/tflite-micro/blob/tinyml-petebook-baseline/tensorflow/lite/micro/examples/hello_world/train/train_hello_world_model.ipynb
+
+# Define paths to model files
+import os
+MODELS_DIR = 'models/'
+if not os.path.exists(MODELS_DIR):
+    os.mkdir(MODELS_DIR)
+MODEL_TF = MODELS_DIR + 'model'
+MODEL_NO_QUANT_TFLITE = MODELS_DIR + 'model_no_quant.tflite'
+MODEL_TFLITE = MODELS_DIR + 'model.tflite'
+MODEL_TFLITE_MICRO = MODELS_DIR + 'model.cc'
+
 
 # TensorFlow is an open source machine learning library
 import tensorflow as tf
-import os
+
 # Keras is TensorFlow's high-level API for deep learning
 from tensorflow import keras
 # Numpy is a math library
 import numpy as np
-# Pandas is a data manipulation library
+# Pandas is a data manipulation library 
 import pandas as pd
 # Matplotlib is a graphing library
 import matplotlib.pyplot as plt
@@ -18,6 +29,7 @@ import math
 seed = 1
 np.random.seed(seed)
 tf.random.set_seed(seed)
+
 # Number of sample datapoints
 SAMPLES = 1000
 
@@ -31,7 +43,6 @@ np.random.shuffle(x_values)
 
 # Calculate the corresponding sine values
 y_values = np.sin(x_values).astype(np.float32)
-
 
 # Add a small random number to each y value
 y_values += 0.1 * np.random.randn(*y_values.shape)
@@ -50,7 +61,7 @@ y_train, y_test, y_validate = np.split(y_values, [TRAIN_SPLIT, TEST_SPLIT])
 # Double check that our splits add up correctly
 assert (x_train.size + x_validate.size + x_test.size) ==  SAMPLES
 
-
+# We'll use Keras to create a simple model architecture
 model_1 = tf.keras.Sequential()
 
 # First layer takes a scalar input and feeds it through 8 "neurons". The
@@ -67,6 +78,7 @@ model_1.compile(optimizer='adam', loss='mse', metrics=['mae'])
 history_1 = model_1.fit(x_train, y_train, epochs=500, batch_size=64,
                         validation_data=(x_validate, y_validate))
 
+
 # Draw a graph of the loss, which is the distance between
 # the predicted and actual values during training and validation.
 train_loss = history_1.history['loss']
@@ -74,31 +86,53 @@ val_loss = history_1.history['val_loss']
 
 epochs = range(1, len(train_loss) + 1)
 
+# plt.plot(epochs, train_loss, 'g.', label='Training loss')
+# plt.plot(epochs, val_loss, 'b', label='Validation loss')
+# plt.title('Training and validation loss')
+# plt.xlabel('Epochs')
+# plt.ylabel('Loss')
+# plt.legend()
+# plt.show()
+
+# Exclude the first few epochs so the graph is easier to read
+SKIP = 50
+
+# plt.plot(epochs[SKIP:], train_loss[SKIP:], 'g.', label='Training loss')
+# plt.plot(epochs[SKIP:], val_loss[SKIP:], 'b.', label='Validation loss')
+# plt.title('Training and validation loss')
+# plt.xlabel('Epochs')
+# plt.ylabel('Loss')
+# plt.legend()
+# plt.show()
+
+# plt.clf()
+
 # Draw a graph of mean absolute error, which is another way of
 # measuring the amount of error in the prediction.
 train_mae = history_1.history['mae']
 val_mae = history_1.history['val_mae']
 
+# plt.plot(epochs[SKIP:], train_mae[SKIP:], 'g.', label='Training MAE')
+# plt.plot(epochs[SKIP:], val_mae[SKIP:], 'b.', label='Validation MAE')
+# plt.title('Training and validation mean absolute error')
+# plt.xlabel('Epochs')
+# plt.ylabel('MAE')
+# plt.legend()
+# plt.show()
+
+# Calculate and print the loss on our test dataset
 test_loss, test_mae = model_1.evaluate(x_test, y_test)
 
 # Make predictions based on our test dataset
 y_test_pred = model_1.predict(x_test)
 
-test_loss, test_mae = model_1.evaluate(x_test, y_test)
-
-# Make predictions based on our test dataset
-y_test_pred = model_1.predict(x_test)
-
-
-# Define paths to model files
-import os
-MODELS_DIR = 'models/'
-if not os.path.exists(MODELS_DIR):
-    os.mkdir(MODELS_DIR)
-MODEL_TF = MODELS_DIR + 'model'
-MODEL_NO_QUANT_TFLITE = MODELS_DIR + 'model_no_quant.tflite'
-MODEL_TFLITE = MODELS_DIR + 'model.tflite'
-MODEL_TFLITE_MICRO = MODELS_DIR + 'model.cc'
+# Graph the predictions against the actual values
+# plt.clf()
+# plt.title('Comparison of predictions and actual values')
+# plt.plot(x_test, y_test, 'b.', label='Actual values')
+# plt.plot(x_test, y_test_pred, 'r.', label='TF predictions')
+# plt.legend()
+# plt.show()
 
 model = tf.keras.Sequential()
 
@@ -115,14 +149,79 @@ model.add(keras.layers.Dense(1))
 # Compile the model using the standard 'adam' optimizer and the mean squared error or 'mse' loss function for regression.
 model.compile(optimizer='adam', loss="mse", metrics=["mae"])
 
+
 # Train the model
 history = model.fit(x_train, y_train, epochs=500, batch_size=64,
                     validation_data=(x_validate, y_validate))
 
 # Save the model to disk
-model.save(MODEL_TF) # wrong - baseline
+model.save(MODEL_TF)
 
+# Draw a graph of the loss, which is the distance between
+# the predicted and actual values during training and validation.
+train_loss = history.history['loss']
+val_loss = history.history['val_loss']
 
+epochs = range(1, len(train_loss) + 1)
+
+# Exclude the first few epochs so the graph is easier to read
+SKIP = 100
+
+# plt.figure(figsize=(10, 4))
+# plt.subplot(1, 2, 1)
+
+# plt.plot(epochs[SKIP:], train_loss[SKIP:], 'g.', label='Training loss')
+# plt.plot(epochs[SKIP:], val_loss[SKIP:], 'b.', label='Validation loss')
+# plt.title('Training and validation loss')
+# plt.xlabel('Epochs')
+# plt.ylabel('Loss')
+# plt.legend()
+
+# plt.subplot(1, 2, 2)
+
+# Draw a graph of mean absolute error, which is another way of
+# measuring the amount of error in the prediction.
+train_mae = history.history['mae']
+val_mae = history.history['val_mae']
+
+# plt.plot(epochs[SKIP:], train_mae[SKIP:], 'g.', label='Training MAE')
+# plt.plot(epochs[SKIP:], val_mae[SKIP:], 'b.', label='Validation MAE')
+# plt.title('Training and validation mean absolute error')
+# plt.xlabel('Epochs')
+# plt.ylabel('MAE')
+# plt.legend()
+
+# plt.tight_layout()
+
+# Calculate and print the loss on our test dataset
+test_loss, test_mae = model.evaluate(x_test, y_test)
+
+# Make predictions based on our test dataset
+y_test_pred = model.predict(x_test)
+
+# Graph the predictions against the actual values
+# plt.clf()
+# plt.title('Comparison of predictions and actual values')
+# plt.plot(x_test, y_test, 'b.', label='Actual values')
+# plt.plot(x_test, y_test_pred, 'r.', label='TF predicted')
+# plt.legend()
+# plt.show()
+
+# Convert the model to the TensorFlow Lite format without quantization
+converter = tf.lite.TFLiteConverter.from_saved_model(MODEL_TF)
+model_no_quant_tflite = converter.convert()
+
+# Save the model to disk
+open(MODEL_NO_QUANT_TFLITE, "wb").write(model_no_quant_tflite)
+
+# Convert the model to the TensorFlow Lite format without quantization
+converter = tf.lite.TFLiteConverter.from_saved_model(MODEL_TF)
+model_no_quant_tflite = converter.convert()
+
+# Save the model to disk
+open(MODEL_NO_QUANT_TFLITE, "wb").write(model_no_quant_tflite)
+
+# Convert the model to the TensorFlow Lite format without quantization
 converter = tf.lite.TFLiteConverter.from_saved_model(MODEL_TF)
 model_no_quant_tflite = converter.convert()
 
@@ -137,6 +236,8 @@ def representative_dataset():
 converter.optimizations = [tf.lite.Optimize.DEFAULT]
 # Enforce integer only quantization
 converter.target_spec.supported_ops = [tf.lite.OpsSet.TFLITE_BUILTINS_INT8]
+converter.inference_input_type = tf.int8
+converter.inference_output_type = tf.int8
 # Provide a representative dataset to ensure we quantize correctly.
 converter.representative_dataset = representative_dataset
 model_tflite = converter.convert()
@@ -144,6 +245,13 @@ model_tflite = converter.convert()
 # Save the model to disk
 open(MODEL_TFLITE, "wb").write(model_tflite)
 
-print(MODEL_TFLITE)
+# Save the model to disk
+open(MODEL_TFLITE, "wb").write(model_tflite)
+
 import os
-print(os.getcwd())
+os.system('xxd -i ' + MODEL_TFLITE + ' > ' + MODEL_TFLITE_MICRO)
+REPLACE_TEXT = MODEL_TFLITE.replace('/', '_').replace('.', '_')
+
+# sed -i 's/'{REPLACE_TEXT}'/g_model/g' {MODEL_TFLITE_MICRO}
+os.system("sed -i 's/" + REPLACE_TEXT + "/g_model/g' " + MODEL_TFLITE_MICRO)
+os.system("cat " + MODEL_TFLITE_MICRO)
